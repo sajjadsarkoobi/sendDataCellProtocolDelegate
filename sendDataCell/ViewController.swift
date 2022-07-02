85a0f6ed895ca6e4e495402f78ea44fe8a63a2d9
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-07-02 10:07:59 +0300

    Initial Commit

---
+++ sendDataCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sendDataCell
+//
+//  Created by Sajjad Sarkoobi on 2.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
